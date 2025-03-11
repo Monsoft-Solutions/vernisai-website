2317c8782f8a72beadf4260547c6b1d2501f42c1
Adriano Flechilla <adrianof.dev@gmail.com>
2025-03-11 18:56:18 -0400

    chore(monorepo): clean up temporary documentation

--- packages/ui/src/badge.ts
+++
@@ -1,9 +0,0 @@
-// This is a placeholder file that will be replaced with the actual badge component
-// when we move components from the website to the UI package
-
-const Badge = () => {
-  // Placeholder
-  return null;
-};
-
-export default Badge;
--- packages/ui/src/button.ts
+++
@@ -1,9 +0,0 @@
-// This is a placeholder file that will be replaced with the actual button component
-// when we move components from the website to the UI package
-
-const Button = () => {
-  // Placeholder
-  return null;
-};
-
-export default Button;
